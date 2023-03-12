c85e72bd5a82bafc149c4c7da717ba9c1be899b2
Saranya Govind <saranya789@gmail.com>
2023-03-12 20:56:36 +0000

    HighCard added

--- src/main/java/org/techreturners/poker/Hand.java
+++ src/main/java/org/techreturners/poker/Hand.java
@@ -52,4 +52,76 @@ public class Hand {
 
     }
 
+    public boolean isPair(Card[] currentHand){
+        //Pair : 2 of the 5 cards in the hand have same value.
+        Card card1 = currentHand[0];
+        Card card2 = currentHand[1];
+        Card card3 = currentHand[2];
+        Card card4 = currentHand[3];
+        Card card5 = currentHand[4];
+        return card1.equals(card2) && !card2.equals(card3) ||
+                (card2.equals(card3) && !card3.equals(card4) && !card2.equals(card1)) ||
+                (card3.equals(card4) && !card4.equals(card5) && !card3.equals(card1)) ||
+                card4.equals(card5) && !card4.equals(card3);
+    }
+
+
+    public boolean isTwoPair(Card[] currentHand){
+        Card card1 = currentHand[0];
+        Card card2 = currentHand[1];
+        Card card3 = currentHand[2];
+        Card card4 = currentHand[3];
+        Card card5 = currentHand[4];
+
+        return ((card1.equals(card2) && card3.equals(card4) && !card2.equals(card3) && !card4.equals(card5)) ||
+                (!card1.equals(card2) && card2.equals(card3) && !card3.equals(card4) && card4.equals(card5)) ||
+                (card1.equals(card2) && card4.equals(card5) && !card2.equals(card3) && !card3.equals(card4))
+        );
+    }
+
+    public boolean isThreeOfAKind(Card[] currentHand){
+        //Three of a Kind:Three of the cards in the hand have same value
+        Card card1 = currentHand[0];
+        Card card2 = currentHand[1];
+        Card card3 = currentHand[2];
+        Card card4 = currentHand[3];
+        Card card5 = currentHand[4];
+
+        return (card1.equals(card3) || card2.equals(card4) || card3.equals(card5));
+    }
+
+
+    public boolean isFullHouse(Card[] currentHand){
+        // Full House: 3 cards of the same value, with  remaining 2 cards forming a pair.
+        Card card1 = currentHand[0];
+        Card card2 = currentHand[1];
+        Card card3 = currentHand[2];
+        Card card4 = currentHand[3];
+        Card card5 = currentHand[4];
+
+        return ((card1.equals(card2) && !card2.equals(card3) && card3.equals(card5)) ||
+                (card1.equals(card3) && !card3.equals(card4) && card4.equals(card5)));
+    }
+    public boolean isStraightFlush(Card[] currentHand){
+        return isStraight(currentHand) && isFlush(currentHand);
+    }
+
+
+    public int compareHighCard(Hand hand1, Hand hand2){
+        Card[] hand1Cards = hand1.getPlayerHand();
+        Card[] hand2Cards = hand2.getPlayerHand();
+        return hand1Cards[hand1Cards.length-1].compareTo(hand2Cards[hand2Cards.length-1]);
+    }
+    private handRank getHandRank(){
+        Card[] tempHandRank = this.getPlayerHand();
+        if (isStraightFlush(tempHandRank)) return handRank.STRAIGHTFLUSH;
+        else if (isFullHouse(tempHandRank)) return handRank.FULLHOUSE;
+        else if (isFlush(tempHandRank)) return handRank.FLUSH;
+        else if (isStraight(tempHandRank)) return handRank.STRAIGHT;
+        else if (isThreeOfAKind(tempHandRank)) return handRank.THREEOFAKIND;
+        else if (isTwoPair(tempHandRank)) return handRank.TWOPAIR;
+        else if (isPair(tempHandRank)) return handRank.PAIR;
+        else return handRank.HIGHCARD;
+    }
+
 }
--- src/test/java/org/techreturners/poker/CardTest.java
+++ src/test/java/org/techreturners/poker/CardTest.java
@@ -40,7 +40,7 @@ public class CardTest  {
 
     @Test
     public void testIsFlush(){
-        Hand handObj = new Hand();;
+        Hand handObj = new Hand();
         Card[] currentHand = {new Card("2C"),new Card("3C"),new Card("4C"),new Card("5C"),new Card("6C")};
         Arrays.sort(currentHand);
         assertTrue(handObj.isFlush(currentHand));
@@ -48,11 +48,52 @@ public class CardTest  {
 
     @Test
     public void testIsStraight(){
-        Hand handObj = new Hand();;
+        Hand handObj = new Hand();
         Card[] currentHand = {new Card("2C"),new Card("3C"),new Card("4C"),new Card("5C"),new Card("6C")};
         Arrays.sort(currentHand);
         assertTrue(handObj.isStraight(currentHand));
     }
 
+
+    @Test
+    public void testIsPair(){
+        Hand handObj = new Hand();
+        Card[] currentHand =  {new Card("2C"),new Card("4D"),new Card("3C"),new Card("5C"),new Card("2D")};
+        Arrays.sort(currentHand);
+        assertTrue(handObj.isPair(currentHand));
+    }
+
+    @Test
+    public void testIsTwoPair(){
+        Hand handObj = new Hand();
+        Card[] currentHand = {new Card("2C"),new Card("4D"),new Card("3C"),new Card("4C"),new Card("2D")};
+        Arrays.sort(currentHand);
+        assertTrue(handObj.isTwoPair(currentHand));
+    }
+
+    @Test
+    public void testIsThreeOfAKind(){
+        Hand handObj = new Hand();
+        Card[] currentHand = {new Card("2C"),new Card("4D"),new Card("3C"),new Card("2H"),new Card("2D")};
+        Arrays.sort(currentHand);
+        assertTrue(handObj.isThreeOfAKind(currentHand));
+    }
+
+    @Test
+    public void testIsFullHouse(){
+        Hand handObj = new Hand();
+        Card[] currentHand = {new Card("2C"),new Card("2H"),new Card("2S"),new Card("5C"),new Card("5D")};
+        Arrays.sort(currentHand);
+        assertTrue(handObj.isFullHouse(currentHand));
+    }
+
+    @Test
+    public void testCompareHighCard(){
+        Hand handObj = new Hand();
+        Hand blackHand = new Hand("4C 2C 3D 6D 2S","black");
+        Hand whiteHand = new Hand("4C 8C 3D 6D 2S","white");
+
+        assertEquals(-1, handObj.compareHighCard(blackHand,whiteHand));
+    }
 
 }
